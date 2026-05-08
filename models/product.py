7ecfd02774255103bf9a9fb3a975f01a913e46a2
yurikses <yurikses@gmail.com>
2026-05-08 11:57:24 +0300

    fix: readme

--- models/product.py
+++ models/product.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
-## Alchemy model for the products table
+## Модель для SQLAchemy
 class Product(Base):
     __tablename__ = "products"
 
--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, EmailStr, Field
 
 class UserProfile(BaseModel):
     username: str
-    age: Annotated[int, Field( gt=18)]
+    age: Annotated[int, Field( gt=18)] ## conint убран в пользу Annotated
     email: EmailStr    
-    password:Annotated[str, Field( min_length=8, max_length=16)] 
+    password:Annotated[str, Field( min_length=8, max_length=16)] ## constr убран в пользу Annotated
     phone: Optional[str] = 'Unknown'
